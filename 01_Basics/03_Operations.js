d7e247c190de4b6b00e5e5dfe77de0caa33748fe
Final year BS Computer Science student | IT enthusiast | Web Developer | YouTube Growth Consultant <102038290+Gulfam7604@users.noreply.github.com>
2024-09-30 13:25:51 +0000

    Operators

---
+++ 01_Basics/03_Operations.js
@@ -0,0 +1,46 @@
+
+let a=10;
+let b=2;
+
+// Arithamatic operator
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a**b);
+console.log(a%b);
+
+// Assignment Operator
+console.log(a==b);
+console.log(a-=b); // a= a-b
+console.log(a+=b); // a= a+b
+console.log(a/=b); // a= a/b
+console.log(a*=b); // a= a*b
+
+
+// Post Inrecrement & Pre Inrecrement
+
+let gamerScore=15;
+
+console.log(gamerScore);
+gamerScore++;
+console.log(gamerScore);
+gamerScore--;
+console.log(gamerScore);
+
+
+// Comparsion Operators;
+
+let x=40;
+let y=30;
+let z='30'
+
+console.log(x==y);
+console.log(x<=y);
+console.log(x>=y);
+console.log(y===z); // Data Type Check
+console.log(y==z);
+console.log(x!=z); 
+
+
+
