59da3ec97b7258f455b777e87c5b5d9b308b73e1
Final year BS Computer Science student | IT enthusiast | Web Developer | YouTube Growth Consultant <102038290+Gulfam7604@users.noreply.github.com>
2024-09-30 11:36:25 +0000

    DataTypes

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,14 @@
+ let accountName="Awais";
+ let age=28;
+let accountStatus= true;
+let city;
+let ammount=null;
+
+
+console.log(typeof age);
+console.log(typeof accountStatus);
+console.log(typeof ammount);
+console.log(typeof city);
+
+
+ 
