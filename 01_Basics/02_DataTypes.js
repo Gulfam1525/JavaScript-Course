056fc646c5db44b06e3af38ebef547d0238af897
Final year BS Computer Science student | IT enthusiast | Web Developer | YouTube Growth Consultant <102038290+Gulfam7604@users.noreply.github.com>
2024-09-30 12:08:07 +0000

    datatypes

--- 01_Basics/02_DataTypes.js
+++ 01_Basics/02_DataTypes.js
@@ -19,5 +19,12 @@ console.log(typeof stringAge);
 console.log(age);
 console.log(typeof age);
 
+let numAccountName= Number(accountName);
+
+console.log(typeof numAccountName); // String can not b changed in Number Data Types
+console.log(numAccountName);
+
+
+
 
  
