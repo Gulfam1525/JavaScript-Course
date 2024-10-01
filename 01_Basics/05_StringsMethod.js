07a96684006bac3f1b4089145bdff77532f696b3
Final year BS Computer Science student | IT enthusiast | Web Developer | YouTube Growth Consultant <102038290+Gulfam7604@users.noreply.github.com>
2024-10-01 05:29:59 +0000

    Strings Method

---
+++ 01_Basics/05_StringsMethod.js
@@ -0,0 +1,27 @@
+// Strings methods
+
+let myName= "gulfam";
+let number = 25;
+
+console.log(myName.length);
+console.log(myName.charAt(3));
+console.log(myName.slice(-2));
+console.log(myName.toUpperCase());
+console.log(myName.trimStart());
+
+console.log(myName.concat(" Kamobh " + number));
+console.log(myName.concat(` kamboh ${number}` ));
+
+console.log(myName.replace("gulfam" , "awais"));
+console.log(myName.repeat(5));
+
+
+
+
+
+
+
+
+
+
+
