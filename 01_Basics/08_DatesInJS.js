6692e07bac6ee489396ddc616fcb510e68151ba5
Final year BS Computer Science student | IT enthusiast | Web Developer | YouTube Growth Consultant <102038290+Gulfam7604@users.noreply.github.com>
2024-10-01 07:55:29 +0000

    Dates In JavaScripts

---
+++ 01_Basics/08_DatesInJS.js
@@ -0,0 +1,25 @@
+
+
+//// Date In JS
+
+
+let mydate = new Date();
+console.log(mydate);
+
+console.log(mydate.toDateString());
+console.log(mydate.toISOString());
+console.log(mydate.toJSON());
+console.log(mydate.toLocaleDateString());
+console.log(mydate.toLocaleTimeString());
+console.log(mydate.toLocaleString());
+
+
+console.log(mydate.getMonth());
+
+let myCreatedDate = new Date('2021-03-10');
+console.log(myCreatedDate.toDateString());
+
+
+
+
+
