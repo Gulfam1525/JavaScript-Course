7b4a76ef35d50f3b9d2134e65580a7d2f1be0e6b
Final year BS Computer Science student | IT enthusiast | Web Developer | YouTube Growth Consultant <102038290+Gulfam7604@users.noreply.github.com>
2024-10-01 10:30:35 +0000

    Arrays

---
+++ 02_Basics/01_Arrays.js
@@ -0,0 +1,39 @@
+
+let myArray = ["Gulfam", "Awais" ,"Sajjad" ,"Usman"]
+
+console.table(myArray);
+
+console.log(myArray.length)
+console.log(myArray.includes("Nadeem"));
+console.log(myArray.push("Nadeem"));
+console.table(myArray);
+console.log(myArray.includes("Nadeem"));
+console.log(myArray[1]);
+
+console.log(myArray.toString());
+console.log(typeof myArray);
+
+console.log(myArray.join(" ahmed "));
+console.log(myArray);
+
+let Langauge = ["C++", "Java" , "Php" , "Python"]
+
+console.log(Langauge.slice(1,3));
+// let newArry = Langauge.slice(1)
+// console.log(newArry);
+
+
+
+
+// console.log(Langauge.splice(0,4));
+
+
+
+
+
+
+
+
+
+
+
